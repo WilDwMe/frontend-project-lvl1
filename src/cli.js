f8dcb501f705470f5da2b0cd72607e32453227d7
WilD <a.nazarenko@nylonfabric.ru>
2021-10-19 21:08:19 +0300

    added GitHub Actions

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,8 @@
 import readlineSync from 'readline-sync';
 
-
 const greeting = () => { 
-    const name = readlineSync.question('Hi! what is your name?  ');
-    console.log(`Hello and nice to meet you ${name}!`);
+  const name = readlineSync.question('Hi! what is your name?  ');
+  console.log(`Hello and nice to meet you ${name}!`);
 };
-
 
 export default greeting;
