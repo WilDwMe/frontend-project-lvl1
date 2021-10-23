eaf37504cc0bfcdd9ea3813c2a004f5c8e61606c
WilD <a.nazarenko@nylonfabric.ru>
2021-10-23 12:41:37 +0300

    add Progression game

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import progress from '../src/Games/progress-game.js';
+
+progress();
---
+++ src/Games/progress-game.js
@@ -0,0 +1,41 @@
+import engine from '../index.js';
+
+const progress = (elem, n) => {
+  const arr = [];
+  for (let i = 0; i < n; i += 1) {
+    arr[i] = elem;
+    elem += n;
+  }
+  return arr;
+};
+
+function check(arr1, arr2) {
+  let num = 0;
+  for (let i = 0; i < arr1.length; i += 1) {
+    if (arr1[i] !== arr2[i]) {
+      num = arr1[i];
+    }
+  }
+  return num;
+}
+
+const progression = () => {
+  const start = Math.round(Math.random() * 10);
+  const index = 5 + Math.round(Math.random() * 5);
+  const arr = progress(start, index);
+  arr[Math.round(Math.random() * index)] = '..';
+  const checking = progress(start, index);
+  const rules = 'What number is missing in the progression?';
+  const exp = arr.join(' ');
+  const rigthAnswer = String(check(checking, arr));
+  console.log(rigthAnswer);
+  return {
+    expression: exp,
+    answer: rigthAnswer,
+    gameRules: rules,
+  };
+};
+
+engine(progression, 0);
+
+export default progression;
