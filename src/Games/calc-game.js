8a3190225f87017912e1e3708df1a41b8bf8ac6f
WilD <a.nazarenko@nylonfabric.ru>
2021-11-01 23:50:44 +0300

    fix question bug

--- src/Games/calc-game.js
+++ src/Games/calc-game.js
@@ -5,7 +5,7 @@ const calc = () => {
   const randomOp = operators[Math.round(Math.random() * 2)];
   const a = Math.round(Math.random() * 10);
   const b = Math.round(Math.random() * 10);
-  const exp = `${a}${randomOp}${b}`;
+  const exp = a + randomOp + b;
   // eslint-disable-next-line no-eval
   const rigthAnswer = String(eval(exp));
   const rules = 'What is the result of the expression?';
